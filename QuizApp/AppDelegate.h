9a0ac7eda36039edeae269de43161fadb4039edc
Moa Lenngren <moa.mail@hotmail.com>
2018-01-24 21:35:21 +0100

    Initial Commit

---
+++ QuizApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QuizApp
+//
+//  Created by ITHS on 2018-01-24.
+//  Copyright © 2018 MoaLenngren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QuizApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QuizApp
+//
+//  Created by ITHS on 2018-01-24.
+//  Copyright © 2018 MoaLenngren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
